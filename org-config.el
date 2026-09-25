0612a428455bec3bbd2cf0189da0d1835773c528
andrmayo <acmayo399@gmail.com>
2026-09-25 01:09:51 -0400

    chore(org-mode): changes to org-mode detailed in previous commit

--- org-config.el
+++ org-config.el
@@ -1,5 +1,5 @@
 ;; automatically wrap lines to fill-column while typing
-(setq-default fill-column 80)
+(setq-default fill-column 100)
 (add-hook 'org-mode-hook #'auto-fill-mode)
 
 (use-package evil-org
@@ -8,11 +8,17 @@
     :diminish evil-org-mode
     :config
     (add-hook 'org-mode-hook #'evil-org-mode)
-    ;; (add-hook 'evil-org-mode-hook
-    ;; 	    (lambda ()
-    ;; 	    (evil-org-set-key-theme)))
+    (add-hook 'evil-org-mode-hook
+	      (lambda ()
+	      (evil-org-set-key-theme '(navigation insert textobjects additional calendar todo))))
     (require 'evil-org-agenda)
     (evil-org-agenda-set-keys))
+
+;; nicer heading bullets/tables/tags/checkboxes than plain org-mode
+(use-package org-modern
+  :ensure t
+  :after org
+  :hook (org-mode . org-modern-mode))
 
 (defun org-latex-preview-all()
   (interactive)
@@ -24,8 +30,24 @@
   :which-key "latex preview all")
 
 (defun org-fill-buffer ()
+  "Fill the buffer, skipping src/example/export/comment blocks."
   (interactive)
-  (fill-region (point-min) (point-max)))
+  (save-excursion
+    (let ((case-fold-search t)
+          (start (copy-marker (point-min)))
+          (end-re "^[ \t]*#\\+end_\\(?:src\\|example\\|export\\|comment\\)")
+          (begin-re "^[ \t]*#\\+begin_\\(?:src\\|example\\|export\\|comment\\)"))
+      (goto-char (point-min))
+      (while (re-search-forward begin-re nil t)
+        (let ((block-beg (copy-marker (line-beginning-position))))
+          (when (> block-beg start)
+            (fill-region start block-beg))
+          (if (re-search-forward end-re nil t)
+              (progn (forward-line 1) (set-marker start (point)))
+            (set-marker start (point-max))
+            (goto-char (point-max)))))
+      (when (< start (point-max))
+        (fill-region start (point-max))))))
 
 (local-leader-def
   :keymaps 'org-mode-map
